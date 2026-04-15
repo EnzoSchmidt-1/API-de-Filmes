aaa4594dd3a1d0e066f791cb7ae4ee5397f61113
Enzo <Enzo.schmidt@edu.unifil.br>
2026-04-15 20:47:05 -0300

    Adicionando o POST do CRUD para possibilitar a adição de filmes

--- index.js
+++ index.js
@@ -27,6 +27,79 @@ app.get('/api/filmes', (req, res) => {
 
 //---------------------------------------------------------------------
 
+//---------------------------------------------------------------------
+//Segunda adição
+
+// GET /api/filmes/:id 
+app.get('/api/filmes/:id', (req, res) => {
+  console.log("verificado com sucesso")
+    try {
+        // 1. Pegar ID da URL
+        const id = parseInt(req.params.id);
+        
+        // 2. Preparar query com WHERE
+        const stmt = db.prepare('SELECT * FROM filmes WHERE id = ?');
+        
+        // 3. Executar (retorna 1 objeto ou undefined)
+        const filme = stmt.get(id);
+        
+        // 4. Verificar se encontrou
+        if (!filme) {
+            return res.status(404).json({ 
+                erro: 'Filme não encontrado' 
+            });
+        }
+        
+        // 5. Retornar filme
+        res.json(filme);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ erro: 'Erro ao buscar filme' });
+    }
+});
+
+// POST 
+app.post('/api/filmes', (req, res) => {
+    try {
+        // 1. Pegar dados do body
+        const { titulo, ano, diretor, genero, nota } = req.body;
+        
+        // 2. Validações (igual antes!)
+        if (!titulo || !ano || !diretor || !genero) {
+            return res.status(400).json({ 
+                erro: 'Campos obrigatórios faltando' 
+            });
+        }
+        
+        if (typeof ano !== 'number' || ano <= 0) {
+            return res.status(400).json({ 
+                erro: 'Ano inválido' 
+            });
+        }
+        
+        // 3. Preparar INSERT
+        const stmt = db.prepare(`INSERT INTO filmes (titulo, ano, diretor, genero, nota) VALUES (?, ?, ?, ?, ?)`);
+        
+        // 4. Executar INSERT
+        const result = stmt.run(titulo, ano, diretor, genero, nota)      
+        // 5. Pegar ID gerado
+        const id = result.lastInsertRowid;
+        
+        // 6. Buscar filme criado (para retornar completo)
+        const filmeCriado = db.prepare(
+            'SELECT * FROM filmes WHERE id = ?'
+        ).get(id);
+        
+        // 7. Retornar 201 Created
+        res.status(201).json(filmeCriado);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ erro: 'Erro ao criar filme' });
+    }
+});
+
+//----------------------------------------------------------------------
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
